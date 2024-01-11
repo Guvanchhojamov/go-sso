28ee9c0f940099ca073cfe084c05f5ef93fea928
Guvanch Hojamov <guvanchhojamov@gmail.com>
2024-01-11 15:51:17 +0500

    added solger helper and Register New User

--- intenal/services/auth/auth.go
+++ intenal/services/auth/auth.go
@@ -2,7 +2,10 @@ package auth
 
 import (
 	"context"
+	"fmt"
 	"go-sso/intenal/domain/models"
+	"go-sso/intenal/lib/logger/sl"
+	"golang.org/x/crypto/bcrypt"
 	"log/slog"
 	"time"
 )
@@ -42,4 +45,26 @@ func (a *Auth) NewAuth(log *slog.Logger, userSaver UserSaver, userProider UserPr
 		appDetails:   appDetails,
 		tokenTTL:     tokenTTL,
 	}
+}
+func (a *Auth) RegisterNewUser(ctx context.Context, email string, password string) (int64, error) {
+	const op = "Auth.RegisterNewUser"
+	log := a.log.With(
+		slog.String("op", op),
+		slog.String("email", email),
+	)
+	log.Info("redistering user")
+
+	//generate password hash
+	passHash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		log.Error("failed to generate password hash", sl.Err(err))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	// Save user to database
+	id, err := a.userSaver.SaveUser(ctx, email, passHash)
+	if err != nil {
+		log.Error("failed save user", sl.Err(err))
+		return 0, fmt.Errorf("%s: %w", op, err)
+	}
+	return id, nil
 }
