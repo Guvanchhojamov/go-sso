1e0063a03bd7327c4726291d56a11e68014aaaf9
Guvanch Hojamov <guvanchhojamov@gmail.com>
2024-01-11 15:31:17 +0500

    added aut service

--- intenal/services/auth/auth.go
+++ intenal/services/auth/auth.go
@@ -1,1 +1,45 @@
 package auth
+
+import (
+	"context"
+	"go-sso/intenal/domain/models"
+	"log/slog"
+	"time"
+)
+
+const (
+	tokenTTL = time.Second * 10
+)
+
+type UserDatabase interface {
+	SaveUser(ctx context.Context, email string, passwordHash []byte) (uid int64, err error)
+	User(ctx context.Context, email string) (models.User, error)
+}
+type UserSaver interface {
+	SaveUser(ctx context.Context, email string, passwordHash []byte) (uid int64, err error)
+}
+
+type UserProvider interface {
+	User(ctx context.Context, email string) (models.User, error)
+}
+type AppDetailsProvider interface {
+	AppDetail(ctx context.Context, appId int) (models.AppDetail, error)
+}
+
+type Auth struct {
+	log          *slog.Logger
+	userSaver    UserSaver
+	userProvider UserProvider
+	appDetails   AppDetailsProvider
+	tokenTTL     time.Duration
+}
+
+func (a *Auth) NewAuth(log *slog.Logger, userSaver UserSaver, userProider UserProvider, appDetails AppDetailsProvider) *Auth {
+	return &Auth{
+		log:          log,
+		userSaver:    userSaver,
+		userProvider: userProider,
+		appDetails:   appDetails,
+		tokenTTL:     tokenTTL,
+	}
+}
